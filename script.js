6c98a11722306a563031b2045abb62110529adfb
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-02 10:31:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let target = document.getElementById("level");
+function getDomLevel(element)
+	{
+		let level = 0;
+		while(element.parentNode)
+			{
+				element = element.parentNode;
+				level++;
+			}
+		return level;
+	}
+
+let domlevel = getDomLevel(target);
+alert("The level of the element is: " + domLevel");
